8e88d858746933823dfb8cc4e6ba3c4bec9a8e83
aj-sison <aj.sison257@gmail.com>
2018-09-12 01:23:19 -0500

    Made DiAngelo

--- state0.js
+++ state0.js
@@ -1,7 +1,10 @@
-var demo = {};
+var demo = {}, centerX = 1500/2, centerY = 1000/2, DiAngelo, speed = 4; // You can assign multiple variables in one line
+// Placing "DiAngelo" there allows it to be accessed universally
 demo.state0 = function(){};
 demo.state0.prototype = {
-    preload: function(){},
+    preload: function(){
+        game.load.image('DiAngelo', 'assets/sprites/DiAngelo.png'); //Load DiAngelo, 1st argument is the key (name), 2nd argument is file location
+    },
     create: function(){
         game.stage.backgroundColor = "#ff0000";
         console.log('state0');
@@ -9,6 +12,11 @@ demo.state0.prototype = {
         addChangeStateEventListeners();
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL; // Game screen will adjust with the window size | Applies to all states
         //There are 3 other scale modes that can be found on the Phaser website
+        
+        DiAngelo = game.add.sprite(centerX, centerY, 'DiAngelo'); //Puts Di into game
+        //game.add.sprite(x-coord., y-coord., key to be loaded)
+        // Places .png's top left corner at the coordinates
+        DiAngelo.anchor.setTo(0.5, 0.5); // Centers Di
 
        // Event Listener
         game.input.keyboard.addKey(Phaser.Keyboard.ONE).onDown.add(changeState, null, null, 1); //: When 1 is pressed, the game will change to state1
@@ -17,7 +25,24 @@ demo.state0.prototype = {
 // Event Listeners are local which means they need to be written in each state
         
     },
-    update: function(){},
+    update: function(){
+        // Move RIGHT
+        if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+            DiAngelo.x += speed;
+        }
+        // Move LEFT
+        else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+            DiAngelo.x -= speed;
+        }
+        // Move UP
+        else if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+            DiAngelo.y -= speed;
+        }
+        // Move DOWN
+        else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+            DiAngelo.y += speed; // "+" is down for y bc (0,0) is at the top left corner
+        }
+    },
 };
 
  // Game will change state when a number is pressed
